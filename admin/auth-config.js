ae5330b6ddf82e4acd5b2d75e563be4e2eb2f01f
png1dev <png1dev@gmail.com>
2026-07-09 19:12:47 -0700

    Simplify admin password change flow

--- admin/auth-config.js
+++ admin/auth-config.js
@@ -1,4 +1,4 @@
 window.ADMIN_AUTH = {
-  username: 'client',
-  password_sha256: '8155e2ce828dc2d2db11500d7708ad6ad82865a305131fec609a5cd31185430c'
+  username: 'admin',
+  password_sha256: '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918'
 };
